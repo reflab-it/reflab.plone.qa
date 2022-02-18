356f9c9b25b80a3eb28e5cf7498d6647c923307e
Francesco Merlo <francesco.merlo@reflab.it>
2022-02-18 17:48:25 +0100

    Base for addon controlpanel

---
+++ src/reflab/plone/qa/browser/controlpanel.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from zope import schema
+from zope.interface import Interface
+
+
+class IQAControlPanel(Interface):
+
+    supervisor_email = schema.TextLine(
+        title=u'Supervisor email',
+        description=u'',
+        default='',
+        required=False,
+    )
+
+
+class QAControlPanelForm(RegistryEditForm):
+    schema = IQAControlPanel
+    schema_prefix = "reflab.plone.qa"
+    label = u'Q&A Settings'
+
+
+QAControlPanelView = layout.wrap_form(
+    QAControlPanelForm, ControlPanelFormWrapper)
