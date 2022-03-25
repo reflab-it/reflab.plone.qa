c369671e21d7b6d556fa5bfc84d6f0026b173173
micheg <michelangelog@gmail.com>
2022-03-25 17:33:21 +0000

    more fields

--- src/reflab/plone/qa/content/qa_comment.py
+++ src/reflab/plone/qa/content/qa_comment.py
@@ -1,61 +1,32 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
+from plone.autoform import directives
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from plone.supermodel.directives import fieldset
+from reflab.plone.qa import _
+from zope import schema
 from zope.interface import implementer
 
-
-# from reflab.plone.qa import _
 
 
 class IQaComment(model.Schema):
     """ Marker interface and Dexterity Python Schema for QaComment
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    fieldset( 'main', label=u'Body', fields=('text', 'author', 'added_at') )
+    fieldset( 'delete', label=u'Delete', fields=('deleted', 'deleted_at', 'deleted_by'))
+    fieldset( 'lock', label=u'Lock', fields=('locked', 'locked_at', 'locked_by'))
 
-    # model.load('qa_comment.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    text = schema.Text( title=_(u'Text'), required=True )
+    author = schema.TextLine( title=_(u'Author'), required=False )
+    added_at = schema.Datetime( title =_(u'Added At'),required=False )
+    approved = schema.Bool( title=_(u'Approved'), required=False )
+    deleted = schema.Bool( title=_(u'Deleted'), required=False )
+    deleted_at = schema.Datetime( title =_(u'Deleted at'),required=False )
+    deleted_by = schema.TextLine( title=_(u'Locked by'), required=False )
+    locked = schema.Bool( title=_(u'Locked'), required=False )
+    locked_at = schema.Datetime( title =_(u'Locked at'),required=False )
+    locked_by = schema.TextLine( title=_(u'Locked by'), required=False )
 
 @implementer(IQaComment)
 class QaComment(Container):
