59bec1542df6343e4e78292b657f9dbfcafd5a2b
micheg <michelangelog@gmail.com>
2022-04-04 22:00:42 +0000

    more fields

--- src/reflab/plone/qa/services/related_objects/get.py
+++ src/reflab/plone/qa/services/related_objects/get.py
@@ -16,6 +16,25 @@ class RelatedObjects(object):
         self.request = request
 
     def __call__(self, expand=False):
+        def get_field(item):
+            return {
+                'id': item.id,
+                'title': item.title,
+                'description': item.description,
+                'author': item.author,
+                'closed': item.closed,
+                'text': item.text,
+                'approved': item.approved,
+                'deleted': item.deleted,
+                'author': item.author,
+                '_meta':
+                {
+                    'type': item.Type(),
+                    'portal_type': item.portal_type
+                },
+                'link': item.absolute_url(),
+                'rel': item.absolute_url(1),
+            }
         result = {
             'related-objects': {
                 '@id': '{}/@related-objects'.format(
@@ -27,28 +46,16 @@ class RelatedObjects(object):
             return result
         contents = [x.getObject() for x in api.content.find(context=self.context, depth=1)]
         tmp = []
-        #import pdb; pdb.set_trace()
+        parent = None
+        if self.context.Type() == 'Question':
+            parent = get_field(self.context)
         for i in contents:
-            tmp.append({
-                'id': i.id,
-                'title': i.title,
-                'description': i.description,
-                'author': i.author,
-                'closed': i.closed,
-                'text': i.text,
-                'approved': i.approved,
-                'deleted': i.deleted,
-                'author': i.author,
-                '_meta':
-                {
-                    'type': i.Type(),
-                    'portal_type': i.portal_type
-                },
-                'link': i.absolute_url(),
-                'rel': i.absolute_url(1),
-            })
+            tmp.append(get_field(i))
         response = {
-            'related-objects': tmp
+            'related-objects': {
+                'item': tmp,
+                'parent': parent,
+            }
         }
         return response
 
@@ -63,6 +70,6 @@ class RelatedObjectsGetQuestions(Service):
 
     def reply(self):
         related_objects = RelatedObjects(self.context, self.request)
-        tmp = related_objects(expand=True)['related-objects']
+        tmp = related_objects(expand=True)['related-objects']['item']
         tmp = [ i for i in tmp if i['_meta']['type'] == 'Question' ]
         return tmp
