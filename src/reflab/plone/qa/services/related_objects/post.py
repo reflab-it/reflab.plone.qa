59b1af2bde052b31c8d631f09a0c56a44b631e42
micheg <michelangelog@gmail.com>
2022-05-31 13:52:43 +0000

    creating reply object

--- src/reflab/plone/qa/services/related_objects/post.py
+++ src/reflab/plone/qa/services/related_objects/post.py
@@ -5,6 +5,11 @@ from plone.restapi.services import Service
 from zope.component import adapter
 from zope.interface import Interface
 from zope.interface import implementer
+from zope.interface import alsoProvides
+from datetime import datetime
+import plone.protect.interfaces
+import json
+import uuid
 
 
 @implementer(IExpandableElement)
@@ -28,30 +33,40 @@ class InsertPost(object):
 class InsertPostObj(Service):
 
     def reply(self):
+        # disable cors
+        if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+            alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
+
         data = InsertPost(self.context, self.request)
         invalid_response = {
             'status': 'error',
             'message': 'invalid data'
         }
-
         _type = None
         _parent_type = None
         _parent_id = None
-
         _data_is_valid = True
+        user_name = None
+
+        post_data = self.request['BODY'] or None
+
+        if api.user.is_anonymous() or post_data == None:
+            return invalid_response
+
+        data = {}
+        response = {}
         try:
-            if self.request.has_key('type'):
-                _type = self.request.get('type')
-            if self.request.has_key('parent_type'):
-                _parent_type = self.request.get('parent_type')
-            if self.request.has_key('parent_id'):
-                _parent_id = self.request.get('parent_id')
+            user_name = api.user.get_current().getUserName()
+            data = json.loads(post_data)
+            _type = data.get('type')
+            _parent_type = data.get('parent_type')
+            _parent_id = data.get('parent_id')
         except:
             _data_is_valid = False
 
         if _data_is_valid is not True:
             return invalid_response
-        
+
         if _type is None or _parent_type is None or _parent_id is None:
             return invalid_response
 
@@ -60,9 +75,25 @@ class InsertPostObj(Service):
         if _type == 'comment':
             pass
 
-        if _type == 'response':
-            pass 
+        if _type == 'reply':
+            # getting base object
+            obj = api.content.find(context=self.context, id=_parent_id)
+            question = obj[0].getObject()
+            try:
+                res = api.content.create(
+                    container=question,
+                    type='qa Answer',
+                    id=str(uuid.uuid4()),
+                    author=user_name,
+                    added_at=datetime.now(),
+                    text=data.get('data') or ''
+                )
+                response['status'] = 'ok'
+                response['message'] = 'created'
+            except:
+                response['status'] = 'error'
+                response['message'] = 'unable to created'
 
         # import pdb; pdb.set_trace()
 
-        return data()
+        return response
