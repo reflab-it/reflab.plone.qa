ebb0f9ce582213868b0309d56c7836ecf2ea59ed
Francesco Merlo <francesco.merlo@reflab.it>
2023-01-12 12:32:35 +0100

    Implemented viewed logic

--- src/reflab/plone/qa/services/question/head_viewed.py
+++ src/reflab/plone/qa/services/question/head_viewed.py
@@ -11,5 +11,11 @@ logger = logging.getLogger("Plone")
 class ViewedHead(Service):
     def reply(self):
         username = user_api.get_current().getUserName()
+        if self.context.Creator() != username:
+            viewed_by = self.context.viewed_by
+            if username not in viewed_by :
+                viewed_by.append(username)
+                setattr(self.context, 'viewed_by', viewed_by)
+                self.context.reindexObject(idxs=['view_count'])
         logger.info(f'User {username} viewed {self.context.absolute_url()}')
         return self.reply_no_content()
